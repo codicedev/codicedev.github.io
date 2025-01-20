91e328c874bbe55a66272e1725bc37922284b6d5
CandiceMak <maklokyi2022@gmail.com>
2025-01-20 16:53:42 +0800

    codice

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,7 @@ var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("menu").style.top = "0";
   } else {
-    document.getElementById("menu").style.top = "-60px";
+    document.getElementById("menu").style.top = "-107px";
   }
   prevScrollpos = currentScrollPos;
 }
